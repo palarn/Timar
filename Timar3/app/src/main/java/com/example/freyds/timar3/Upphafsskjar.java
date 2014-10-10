c6354130ed57d8c26a0b4cdea30d24a4cddffc29
frh12 <freydisll@gmail.com>
2014-10-10 14:00:45 +0000

    Nýtt
    Það sem er komið er að skrá nýja vinnu, hægt að velja vinnurnar frá
    upphafsskjánum og fara inní vinnuna og breyta upplýsingum

---
+++ Timar3/app/src/main/java/com/example/freyds/timar3/Upphafsskjar.java
@@ -0,0 +1,69 @@
+package com.example.freyds.timar3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Upphafsskjar extends Activity implements AdapterView.OnItemClickListener {
+
+    ListView vinnuListi;                    //ListView widget sem heldur utan um öll nöfnin á skráðum vinnum
+    DatabaseAdapter helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upphafsskjar);
+
+        ArrayAdapter<String> adapter;
+        helper = new DatabaseAdapter(this);
+        vinnuListi = (ListView)findViewById(R.id.vinnuListi);
+
+        String[] data = helper.getNames();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        vinnuListi.setAdapter(adapter);
+        vinnuListi.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_upphafsskjar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.ny_vinna) {
+            Intent newJob = new Intent(Upphafsskjar.this, ny_vinna.class);
+            Upphafsskjar.this.startActivity(newJob);
+        }
+        /*else if (id == R.id.yfirlit) {
+            Intent yfirlit = new Intent(Upphafsskjar.this, yfirlit.class);
+            Upphafsskjar.this.startActivity(yfirlit);
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //fall sem skiptir um activity þegar ýtt er á e-a vinnu á listanum
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String name = (String) (vinnuListi.getItemAtPosition(i));           //nafnið á vinnunni
+        Intent job_selected = new Intent(Upphafsskjar.this, Vinna.class);
+        job_selected.putExtra("name", name);                                //sendir nafnið á vinnunni í Vinna activityið
+        Upphafsskjar.this.startActivity(job_selected);
+    }
+}
